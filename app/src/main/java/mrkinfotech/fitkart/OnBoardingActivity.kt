f7d518a51a1c34aca670820842b919b4552b3049
dabhi <dabhigautam1377@gmail.com>
2025-06-08 19:53:54 +0530

    First Commit

---
+++ app/src/main/java/mrkinfotech/fitkart/OnBoardingActivity.kt
@@ -0,0 +1,13 @@
+package mrkinfotech.fitkart
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class OnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        }
+    }
