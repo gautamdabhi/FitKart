245c22a509ebc092404c021d4e2e2ed94841935e
dabhi <dabhigautam1377@gmail.com>
2025-06-09 13:07:14 +0530

    second commit

--- app/src/main/java/mrkinfotech/fitkart/OnBoardingActivity.kt
+++ app/src/main/java/mrkinfotech/fitkart/OnBoardingActivity.kt
@@ -1,13 +1,23 @@
 package mrkinfotech.fitkart
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import mrkinfotech.fitkart.databinding.ActivityOnBoardingBinding
 
 class OnBoardingActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityOnBoardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.buttonGetStarted.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
+}
--- app/src/main/java/mrkinfotech/fitkart/SplashActivity.kt
+++ app/src/main/java/mrkinfotech/fitkart/SplashActivity.kt
@@ -1,6 +1,9 @@
 package mrkinfotech.fitkart
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,9 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, OnBoardingActivity::class.java))
+            finish()
+        }, 2000)
     }
 }
