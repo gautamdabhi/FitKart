57ebea020ce591fd5bf188c70b07c3dc8166567b
rituraj-799 <odedararuturaj@gmail.com>
2025-06-23 22:41:12 +0530

    seven commit

---
+++ app/src/main/java/mrkinfotech/fitkart/ui/login/SignUpFragment.kt
@@ -0,0 +1,23 @@
+package mrkinfotech.fitkart.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import mrkinfotech.fitkart.R
+
+
+class SignUpFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+
+
+    }
+}
