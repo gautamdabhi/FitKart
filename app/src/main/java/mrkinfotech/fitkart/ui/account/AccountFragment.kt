7af942f27c1c4a0ea7f617be11f8d6959428774f
dabhi <dabhigautam1377@gmail.com>
2025-12-07 10:14:45 +0530

    Logout button in action

--- app/src/main/java/mrkinfotech/fitkart/ui/account/AccountFragment.kt
+++ app/src/main/java/mrkinfotech/fitkart/ui/account/AccountFragment.kt
@@ -1,5 +1,6 @@
 package mrkinfotech.fitkart.ui.account
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import mrkinfotech.fitkart.R
 import mrkinfotech.fitkart.databinding.FragmentAccountBinding
+import mrkinfotech.fitkart.ui.base.SplashActivity
+import mrkinfotech.fitkart.utils.PreferenceHelper
 
 class AccountFragment : Fragment() {
 
@@ -23,6 +26,15 @@ class AccountFragment : Fragment() {
 
         _binding = FragmentAccountBinding.inflate(inflater, container, false)
         return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonLogOut.setOnClickListener {
+            PreferenceHelper.setUserEmail(requireContext(), "")
+            startActivity(Intent(requireContext(), SplashActivity::class.java))
+            requireActivity().finish()
+//            findNavController().navigateUp()
+        }
     }
 
 }
